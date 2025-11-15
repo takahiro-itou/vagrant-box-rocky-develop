9d6559fe954a6aa53d1c96a0c6c84426631c2b69
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-11-15 19:25:46 +0900

    パッケージマネージャを yum から dnf に変更

--- vagrant/provision/setup-packages.sh
+++ vagrant/provision/setup-packages.sh
@@ -4,10 +4,20 @@ echo  Provisioning $HOSTNAME
 
 sudo  timedatectl  set-timezone Asia/Tokyo
 
-sudo  yum  update -y
-sudo  yum  groupinstall -y  "Base"  "Development Tools"
-sudo  yum  install -y  cmake  emacs  git  mercurial  \
-    clang  doxygen  screen
+sudo  dnf install -y dnf-plugins-core
+sudo  dnf  update -y
+
+sudo  dnf  groupinstall -y  "Base"  "Development Tools"
+
+sudo  dnf  install -y  \
+    clang       \
+    cmake       \
+    doxygen     \
+    emacs       \
+    git         \
+    mercurial   \
+    screen      \
+;
 
 sudo  mkdir  -p    /tools
 sudo  chmod  1777  /tools
