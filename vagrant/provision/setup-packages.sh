bd9dc832fd1bf240f46ce2c45eb95a08a6840a78
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-11-16 10:30:36 +0900

    不具合修正
    (1) 必要なリポジトリ CRB (旧: PowerTools) を有効化

--- vagrant/provision/setup-packages.sh
+++ vagrant/provision/setup-packages.sh
@@ -4,19 +4,21 @@ echo  Provisioning $HOSTNAME
 
 sudo  timedatectl  set-timezone Asia/Tokyo
 
-sudo  dnf install -y dnf-plugins-core
+sudo  dnf  install -y dnf-plugins-core
 sudo  dnf  update -y
+
+sudo  dnf  config-manager --set-enabled crb
 
 sudo  dnf  groupinstall -y  "Base"  "Development Tools"
 
-sudo  dnf  install -y  \
-    clang       \
-    cmake       \
-    doxygen     \
-    emacs       \
-    git         \
-    mercurial   \
-    screen      \
+sudo  dnf  install -y   \
+    clang               \
+    cmake               \
+    doxygen             \
+    emacs               \
+    git                 \
+    mercurial           \
+    screen              \
 ;
 
 sudo  mkdir  -p    /tools
